e39df920c5b25f88568c8eab4fd4710c5d2cdb4a
Ritesh Kumar <riteshnagwan@gmail.com>
2023-03-27 08:56:54 +0530

    .

--- src/main/java/com/realtime/smartcontactmanager/config/CustomUserDetails.java
+++ src/main/java/com/realtime/smartcontactmanager/config/CustomUserDetails.java
@@ -1,58 +1,58 @@
-// package com.realtime.smartcontactmanager.config;
+package com.realtime.smartcontactmanager.config;
 
-// import java.util.Arrays;
-// import java.util.Collection;
-// import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
-// import org.springframework.security.core.GrantedAuthority;
-// import org.springframework.security.core.authority.SimpleGrantedAuthority;
-// import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-// import com.realtime.smartcontactmanager.entity.User;
+import com.realtime.smartcontactmanager.entity.UserEntity;
 
-// public class CustomUserDetails implements UserDetails {
+public class CustomUserDetails implements UserDetails {
 
-//     private User user;
+    private UserEntity user;
 
-//     public CustomUserDetails(User user) {
-//         super();
-//         this.user = user;
-//     }
+    public CustomUserDetails(UserEntity user) {
+        super();
+        this.user = user;
+    }
 
-//     @Override
-//     public Collection<? extends GrantedAuthority> getAuthorities() {
-//         var simpleGrantedAuthorities = Arrays.stream(user.getRoles().split(",")).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
-//         return simpleGrantedAuthorities;
-//     }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        var simpleGrantedAuthorities = Arrays.stream(user.getRoles().split(",")).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+        return simpleGrantedAuthorities;
+    }
 
-//     @Override
-//     public String getPassword() {
-//         return user.getPassword();
-//     }
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
 
-//     @Override
-//     public String getUsername() {
-//         return user.getName();
-//     }
+    @Override
+    public String getUsername() {
+        return user.getName();
+    }
 
-//     @Override
-//     public boolean isAccountNonExpired() {
-//         return true;
-//     }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
-//     @Override
-//     public boolean isAccountNonLocked() {
-//         return true;
-//     }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
 
-//     @Override
-//     public boolean isCredentialsNonExpired() {
-//         return true;
-//     }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 
-//     @Override
-//     public boolean isEnabled() {
-//         return user.isEnabled();
-//     }
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
     
-// }
+}
